b4c20d9f04a4d7a78e85658df9fb961ad94a8756
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2023-01-17 18:17:59 -0500

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,25 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyAncOnTF6BKyOwew27VdsQ4YG_ejL5YUgM",
+  authDomain: "react-chat-c42eb.firebaseapp.com",
+  projectId: "react-chat-c42eb",
+  storageBucket: "react-chat-c42eb.appspot.com",
+  messagingSenderId: "664721856323",
+  appId: "1:664721856323:web:4058af046fd0a43caa01f6",
+  measurementId: "G-YP304D35R7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
